ffcdfd31b0481107b05e121d52652fa4ee3cc52b
sophiakwon <skwon2@cca.edu>
2018-04-17 16:12:13 -0700

    .

--- um/js/scripts.js
+++ um/js/scripts.js
@@ -10,18 +10,14 @@ $(document).ready(function(){
     
     
     scrollorama.animate('.mask1',{
-      delay:0, duration:3500, property:'opacity', start:1, end:0.3
-    });
-
-    scrollorama.animate('.comic',{
-      delay:0, duration:500, property:'opacity', start:0, end:1
+      delay:0, duration:8000, property:'zoom', start:1, end:220
     });
 
     
+
+    
  
-    scrollorama.animate('.mask2',{
-      delay:2500, duration:3500, property:'opacity', start:0.3, end:1
-    });
+    
 
   
   
@@ -35,7 +31,7 @@ $(document).ready(function(){
     });
 
      scrollorama.animate('.back2',{
-      delay:7200, duration:993, property:'opacity', start:0, end:1
+      delay:7000, duration:993, property:'opacity', start:0, end:1
     });
      
      scrollorama.animate('.fire',{
@@ -46,26 +42,24 @@ $(document).ready(function(){
       delay:8400, duration:293, property:'right', start:-1500, end:400
     });
 
-     scrollorama.animate('.aboutartist',{
-      delay:9188, duration:700, property:'bottom', start:0, end:700
-    });
+    
 
      scrollorama.animate('.hand1',{
-      delay:11000, duration:1093, property:'left', start:-800, end:-10
+      delay:10000, duration:1093, property:'left', start:-800, end:-10
     });
 
      scrollorama.animate('.hand2',{
-      delay:11000, duration:1093, property:'right', start:-800, end:-10
+      delay:10000, duration:1093, property:'right', start:-800, end:-10
     });
     
    
     
     scrollorama.animate('.exhibitiontag',{
-      delay:12230, duration:993, property:'bottom', start:0, end:993
+      delay:12230, duration:1093, property:'bottom', start:0, end:993
     });
 
      scrollorama.animate('.bullet',{
-      delay:14030, duration:2093, property:'zoom', start:0.04, end:1.5
+      delay:14030, duration:2093, property:'zoom', start:0.04, end:5
     });
 
      scrollorama.animate('.varoom',{
@@ -76,13 +70,7 @@ $(document).ready(function(){
       delay:14180, duration:93, property:'opacity', start:1, end:0
     });
 
-      scrollorama.animate('.artworkused',{
-      delay:16000, duration:900, property:'bottom', start:0, end:900
-    });
-
-scrollorama.animate('.credit',{
-      delay:17700, duration:100, property:'bottom', start:-200, end:-50
-    });
+   
 
    
  
