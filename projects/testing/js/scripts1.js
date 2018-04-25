f17a7c1079b3d83365920554db6c5960b424ee52
sophiakwon <skwon2@cca.edu>
2018-04-24 18:18:45 -0700

    l

---
+++ projects/testing/js/scripts1.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+
+  $('.curtains>li').css('position', 'fixed');
+
+   $('.curtains').curtain();
+   
+  var scrollorama = $.scrollorama({
+    blocks:'.curtains'
+  });
+    
+
+    
+    scrollorama.animate('.plane',{
+      delay:7600, duration:993, property:'left', start:-1500, end:-100
+    });
+     
+    scrollorama.animate('.cloud',{
+      delay:0, duration:993, property:'left', start:-600, end:500
+    });
+
+     scrollorama.animate('.back2',{
+      delay:0, duration:993, property:'opacity', start:0, end:1
+    });
+     
+     scrollorama.animate('.fire',{
+      delay:7600, duration:993, property:'right', start:-1500, end:-200
+    });
+     
+     scrollorama.animate('.whaam',{
+      delay:8400, duration:293, property:'right', start:-1500, end:400
+    });
+
+    
+
+     scrollorama.animate('.hand1',{
+      delay:10000, duration:1093, property:'left', start:-800, end:-10
+    });
+
+     scrollorama.animate('.hand2',{
+      delay:10000, duration:1093, property:'right', start:-800, end:-10
+    });
+    
+   
+    
+    scrollorama.animate('.exhibitiontag',{
+      delay:12230, duration:1093, property:'bottom', start:0, end:993
+    });
+
+     
+
+   
+ 
+   
+   
+});
+
+
+
+    $(document).ready(function(){
+
+      $(".arrow").click(function(){
+
+        $("html, body"). animate({
+
+          scrollTop: $(".endpoint").offset().top
+
+        },3000);
+
+      })
+
+
+    });
+        
